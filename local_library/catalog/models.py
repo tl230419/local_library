f2b0722d9ceed85292a2cddfa2c544ffa43d2242
Allen <tl230419@163.com>
2020-06-14 16:13:34 +0800

    keep learn new chapters.

--- local_library/catalog/models.py
+++ local_library/catalog/models.py
@@ -13,6 +13,13 @@ class Genre(models.Model):
         """
         return self.name
 
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
+
+    def __str__(self):
+        """String for representing the Model object (in Admin site etc.)"""
+        return self.name
 
 from django.urls import reverse
 
@@ -25,10 +32,13 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file.
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    #isbn = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    #language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """
--- local_library/catalog/views.py
+++ local_library/catalog/views.py
@@ -12,9 +12,37 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact = 'a').count()
     num_authors = Author.objects.count() # The 'all()' is implied by default.
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors},
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},  # num_visits appended
     )
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    # context_object_name = 'my_book_list'
+    # queryset = Book.objects.filter(title__icontains='war')[:5]
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+    #
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]
+    #
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     # Call the base implementation first to get the context
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Create any data and add it to the context
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
